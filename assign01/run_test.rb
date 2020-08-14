c5609aac3013f9c6c6a5dd26c33cfb1665e47f86
David Hovemeyer <david.hovemeyer@gmail.com>
2020-08-14 16:54:10 -0400

    run the test

--- assign01/run_test.rb
+++ assign01/run_test.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+require 'open3'
+
 # Run a test for assignment 1.
 # The ASSIGN01_DIR environment variable must be set to the directory
 # containing the "minicalc" executable.
@@ -17,4 +19,22 @@ raise "#{exe} is not executable" if !FileTest.executable?(exe)
 raise "Usage: ./run_test.rb <testname>" if ARGV.length != 1
 testname = ARGV.shift
 
-puts "TOOD: stuff"
+# Make sure that input file exists (otherwise, the test doesn't exist)
+input_file = "input/#{testname}.in"
+raise "No such test input #{input_file}" if !(FileTest.exist?(input_file) && FileTest.readable?(input_file))
+
+# Run the executable on the named test
+cmd = [exe, input_file]
+puts "cmd is: #{cmd.join(' ')}"
+stdout_str, stderr_str, status = Open3.capture3(*cmd, stdin_data: '')
+if !status.success?
+  puts "Test command failed"
+else
+  # write output to actual_output directory
+  Dir.mkdir('actual_output') if !FileTest.directory?('actual_output')
+  File.open("actual_output/#{testname}.out", 'w') do |out|
+    out.print stdout_str
+  end
+
+  puts "Brap!"
+end
